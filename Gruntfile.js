a065bcdddf6a7021540fa5f372ef699cc003ec86
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-02-22 11:01:03 -0800

    Add test task

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,4 +67,5 @@ module.exports = function(grunt) {
 
   grunt.registerTask("default", ["concat", "browserify"]);
   grunt.registerTask("develop", ["default", "exec:refresh_browser"])
+  grunt.registerTask("test",    ["exec:mocha"])
 };
